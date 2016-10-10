0c6a4c3f43503ade3b9640f6b89a2973a6f5c5b6
Nikhil Hegde <nikhilmhegde@gmail.com>
2016-10-10 02:36:28 +0000

    get_redis.py deleted online with Bitbucket

--- get_redis.py
+++
@@ -1,8 +0,0 @@
-import pickle
-import redis
-
-r = redis.StrictRedis(host='localhost', port=6379, db=0)
-news = pickle.loads(r.get('news'))
-
-for article in news.articles:
-    print article.source, article.sortBy, len(article.articles)
